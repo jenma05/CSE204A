08650b5af6ccc1c72d2d180ee8a46cf4f5844f1a
Jennifer Martinez Mejia <j.h.martinezmejia@wustl.edu>
2025-04-01 22:26:41 -0500

    added the key as <li key={move}> to Game function

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -86,7 +86,7 @@ export default function Game() {
       description = 'Go to game start';
     }
     return (
-      <li>
+      <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
     );
