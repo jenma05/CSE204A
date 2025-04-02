c3baa596555b0f75ad8702d3c493b2b3b46469c4
Jennifer Martinez Mejia <j.h.martinezmejia@wustl.edu>
2025-04-01 22:15:15 -0500

    adding a check to the handleClick if the square is already filled

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -25,6 +25,9 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }v
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = "X";
