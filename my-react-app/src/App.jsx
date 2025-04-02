9e3e3443926951088e14b3c1f506a20d4afe4d05
Jennifer Martinez Mejia <j.h.martinezmejia@wustl.edu>
2025-04-01 22:04:46 -0500

    changing back the Square component to receive the value prop from the Board component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -12,21 +12,8 @@ import './App.css'
 //   )
 // }
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue('X');
-  }
-
-  return (
-    <button
-      className="square"
-      onClick={handleClick}
-    >
-      {value}
-    </button>
-  );
+function Square({value}) {
+  return <button className="square">{value}</button>;
 }
 
 export default function Board() {
